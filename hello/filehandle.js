89215a80d5299d9217d4d51f7d282e54a6a6322f
ASHWANEETH <ashwaneeth@gmail.com>
2024-10-19 13:30:05 +0530

    node arch

--- hello/filehandle.js
+++ hello/filehandle.js
@@ -1,7 +1,19 @@
 const fs = require("fs");
 
+
+// client req > event queue > event loop >
+          // if non blocking > process and respond
+          // if blocking > process to thread pool (threads = workers) > respond
+
+
+
+
+//blocking  
 // fs.writeFileSync("./example1.txt","writing file with fs"
 // )
+
+//non blocking  
+// fs.writeFile("./example1.txt","writing file with fs");
 
 // fs.appendFileSync("./example1.txt","\n appending data")
 
