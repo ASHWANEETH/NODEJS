d01fdb34ca1b374a77fd393aaf823f062cbe5f78
ASHWANEETH <ashwaneeth@gmail.com>
2024-10-19 13:08:07 +0530

    file handling

---
+++ hello/filehandle.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.writeFileSync("./example1.txt","writing file with fs"
+// )
+
+// fs.appendFileSync("./example1.txt","\n appending data")
+
+// console.log(fs.readFileSync("./example1.txt","utf-8"));
+
+// fs.mkdirSync("newDirWithFS")
+
