c66f67d23be84f1f0923e740ed358323d9aff404
ASHWANEETH <ashwaneeth@gmail.com>
2024-11-08 15:53:48 +0530

    url

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 const server = http.createServer((req,res)=>{
-  let logs = `${Date.now()} request recieved ${req.url} \n`;
-  fs.appendFile("log.txt",logs,(data,err)=>{res.end("Hi Brooo");})
-
+  if(req.url === '/favicon.ico') return res.end();
+  const Murl = url.parse(req.url,true);
+  let logs = `${Date.now()} request recieved ${Murl.pathname} and username = ${Murl.query.username} \n`;
+  fs.appendFile("log.txt",logs,(data,err)=>{res.end(`Hi ${Murl.query.username}`);})
+  console.log(Murl);
 });
 
 server.listen(8000,() => {})
